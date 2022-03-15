3f69200fb974d04c5fb90e0710a530fdd8889412
Elias <eliasnemr@gmail.com>
2022-03-15 18:56:26 +0200

    Update files&.gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/eliasnemr/projects/documentation/minidocs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/eliasnemr/projects/documentation/minidocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/eliasnemr/projects/documentation/minidocs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/eliasnemr/projects/documentation/minidocs/src/css/custom.css'),
-];
