bb6cbf4753eb713203ebc50d0bc95befb399b01a
jazminima <jazmin.ribeiro@minima.global>
2024-01-08 10:50:49 +0000

    Deploy website - based on d1ae6d8ac644ab8dc4242312fe817109b48546b6

---
+++ assets/js/2345397e.a5e90b13.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkminima_docs=self.webpackChunkminima_docs||[]).push([[4282],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/35eb72cb.3e817ffe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkminima_docs=self.webpackChunkminima_docs||[]).push([[3343],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
