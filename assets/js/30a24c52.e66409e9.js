d7fc4180a4953b20305a15af940824c37f68beda
jazminima <jazmin.ribeiro@minima.global>
2024-01-11 13:50:16 +0000

    Deploy website - based on d273135502e6e76497492aae8080e67e237f714c

--- assets/js/30a24c52.e66409e9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkminima_docs=self.webpackChunkminima_docs||[]).push([[453],{4762:l=>{l.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
---
+++ assets/js/5e95c892.b622c45a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkminima_docs=self.webpackChunkminima_docs||[]).push([[9661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(7294);var a=r(6905),c=r(1944),n=r(5281),u=r(8790),i=r(8851),d=r(5893);function t(e){return(0,d.jsx)(c.FG,{className:(0,a.Z)(n.k.wrapper.docsPages),children:(0,d.jsx)(i.Z,{children:(0,u.H)(e.route.routes)})})}}}]);
